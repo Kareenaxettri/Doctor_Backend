b919af266856699610d62dfa18ee46a86e1465f3
Karina <karina@gmail.com>
2026-07-28 23:23:01 +0545

    Add src/models/user.model.ts

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,11 +2,12 @@ import mongoose, { Schema, Document } from "mongoose";
 import { UserType } from "../types/user.type";
 
 export interface IUser extends UserType, Document {
-    // can add mongo related attr
     _id: mongoose.Types.ObjectId;
     createdAt: Date;
     updatedAt: Date;
     role?: "admin" | "user";
+    resetPasswordToken?: string | null;
+    resetPasswordExpires?: Date | null;
 }
 const UserMongoSchema: Schema = new Schema<IUser>(
     {
@@ -17,14 +18,16 @@ const UserMongoSchema: Schema = new Schema<IUser>(
         gender: { type: String, required: true },
         password: { type: String, required: true },
         role: { type: String, enum: ["admin", "user"], default: "user" },
-        profileImage: { type: String, required: false, default: null }
+        profileImage: { type: String, required: false, default: null },
+        resetPasswordToken: { type: String, default: null },
+        resetPasswordExpires: { type: Date, default: null },
     },
     {
-        timestamps: true // createdAt and updatedAt will be automatically added and managed by mongoose
+        timestamps: true
     }
 )
 export const UserModel = mongoose.model<IUser>
 (
-    "User", // db.users -> Model Name "User"
+    "User",
     UserMongoSchema
 );
